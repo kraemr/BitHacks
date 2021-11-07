7760465060e325a0896a9604a19b057a57d658fd
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 13:23:31 +0100

    Update Next_Perm_BSF.h

--- Next_Perm_BSF.h
+++ Next_Perm_BSF.h
@@ -1,13 +1,7 @@
 #include "BSF.h"
-//TODO TEST THIS CODE
 int nextnumpermutation(unsigned int num) 
 {
 unsigned int t = num | (num -1 );
 return num = (t+1) | ((~t&-(~t))-1)>>(BSF(num)+1);
 }
-/*
-Keep in mind, that you can also use this algorrithm to find permutations of anything that is a string of bits, youll just have to utillize differently
-if you are interested, thenjust look up Next lexicographic permutation on Google or smth
-Have Fun
-*/
 
