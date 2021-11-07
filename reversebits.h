fc7a0a244025077f2cb3d3104742eaea28f4b87d
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 14:20:44 +0100

    Update reversebits.h

--- reversebits.h
+++ reversebits.h
@@ -1,9 +1,10 @@
 #include <limits.h>
 
-unsigned int r; // r will be reversed bits of v; first get LSB of v
-int s;
+
 unsigned int reverse_bits(unsigned int v)
 {
+  unsigned int r; // r will be reversed bits of v; first get LSB of v
+int s;
 s = sizeof(v) * CHAR_BIT - 1; // extra shift needed at end
 for (v >>= 1; v; v >>= 1)
 {   
