6ce38fbff222609405b172374ffa3f766b0f4244
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 13:22:33 +0100

    Update Count_set_bits.h

--- Count_set_bits.h
+++ Count_set_bits.h
@@ -1,6 +1,6 @@
-unsigned int c; // count the number of bits set in v
 unsigned int bit32_count_of_bits(unsigned int v)
-{    
+{
+    unsigned int c; // count the number of bits set in v
     for (c = 0; v; c++)
     {
     v &= v - 1; // clear the least significant bit set
@@ -9,6 +9,7 @@ unsigned int bit32_count_of_bits(unsigned int v)
 
 unsigned int bit64_count_of_bits(unsigned int v)
 {
+    unsigned int c; // count the number of bits set in v
 // option 3, for at most 32-bit values in v:
 c =  ((v & 0xfff) * 0x1001001001001ULL & 0x84210842108421ULL) % 0x1f;
 c += (((v & 0xfff000) >> 12) * 0x1001001001001ULL & 0x84210842108421ULL) % 
