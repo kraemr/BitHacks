f30d8cf0b158e4f1233bb03b2ca1bc6d27a16563
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 13:23:08 +0100

    Update FindMax_Min.h

--- FindMax_Min.h
+++ FindMax_Min.h
@@ -1,14 +1,15 @@
 
-int result;
 
 int max(int x, int y)
 {
+  int result;
 result= x ^ ((x ^ y) & - (x < y));
 return result;
 }
 
 int min(int x, int y)
 {
+  int result;
 result = y ^ ((x ^ y) & -(x < y));
 return result;
 }
