79fc8060f1c503af684adac6f9ab2130075e2c85
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 13:25:31 +0100

    Update absval.h

--- absval.h
+++ absval.h
@@ -1,11 +1,10 @@
 
 
 #include <limits.h>
-unsigned int r;
-int v;
 
 unsigned int absolute_value(int v)
 {
+    unsigned int r;
     int const mask = v >> sizeof(int) * CHAR_BIT - 1;
     r = (v ^mask) - mask;
     return r; // the absoulute value
