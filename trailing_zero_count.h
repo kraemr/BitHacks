0b9e0eb45caa40cb0ae02595a898d2cdaedd8636
Robin Krämer <93094049+CodeadinC@users.noreply.github.com>
2021-11-07 14:25:23 +0100

    Update trailing_zero_count.h

--- trailing_zero_count.h
+++ trailing_zero_count.h
@@ -1,9 +1,10 @@
 #include <limits.h>
-int c;  // output: c will count v's trailing zero bits,
-        // so if v is 1101000 (base 2), then c will be 3
 
 int count_trailing_zero(unsigned int v)
 {
+int c;  // output: c will count v's trailing zero bits,
+        // so if v is 1101000 (base 2), then c will be 3
+
 if (v)
 {
   v = (v ^ (v - 1)) >> 1;  // Set v's trailing 0s to 1s and zero rest
